6488ca5a44c5d2f2642ef9de97f3261f78f7cdcc
Brett <bmiller@om1.com>
2023-03-22 17:31:32 -0400

    cleanup output directory

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from jinja2 import Environment, FileSystemLoader
 
 max_score = 100
 test_name = "Python Challenge"
 students = [
-    {"name": "Sandrine",  "score": 100},
+    {"name": "Sandrine", "score": 100},
     {"name": "Gergeley", "score": 87},
     {"name": "Frieda", "score": 92},
 ]
@@ -12,12 +14,13 @@ environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("message.txt")
 
 for student in students:
-    filename = f"message_{student['name'].lower()}.txt"
+    filename = f"out/message_{student['name'].lower()}.txt"
     content = template.render(
         student,
         max_score=max_score,
         test_name=test_name
     )
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, mode="w", encoding="utf-8") as message:
         message.write(content)
         print(f"... wrote {filename}")
