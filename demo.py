206d653624d1d01adcdf6ca27e1f1799c7b8ee24
danielolsen <dolsen@om1.com>
2023-03-24 12:49:44 -0400

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,17 @@
+from noteextractiontemplater import NoteExtractionTemplater
+
+# Demo Config
+config = {
+    "variable_name": "EASI",
+    "source": "note_extraction",
+    "profile_store_staging_source": "'enriched_profile_store_staging_extractions'",
+    "staging_schema_name": "'PROFILE_STORE_STAGING'",
+    "note_extract_table": "'easi__extraction__single_note'",
+    "note_derivation_id": "35",
+    "note_clean_table": "'note_clea'n",
+    "occurrence_date_origin": "'recorded date'",
+    "variable_type": "'EASI - Eczema Area and Severity Index'",
+}
+
+templater = NoteExtractionTemplater()
+templater.write_models(config)
