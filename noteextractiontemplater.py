28526052079d9972a8821b7031162cfa74e8e08b
danielolsen <dolsen@om1.com>
2023-03-24 13:31:20 -0400

    Collapse different FileSystemLoaders - it just works with subdirectories

--- noteextractiontemplater.py
+++ noteextractiontemplater.py
@@ -3,19 +3,11 @@ from jinja2 import Environment, FileSystemLoader
 
 
 class NoteExtractionTemplater(Templater):
-    setup_environment = Environment(loader=FileSystemLoader("templates/note_extraction/setup/"))
-    # final_environment = Environment(loader=FileSystemLoader("templates/note_extraction/final/"))
+    environment = Environment(loader=FileSystemLoader("templates/note_extraction/"))
 
     def write_models(self, config):
-        setup_templates = self.setup_environment.list_templates()
-        # final_templates = self.final_environment.list_templates()
+        templates = self.environment.list_templates()
 
-        # TODO: Somehow collapse these together. Nontrivial because we want to make sure
-        # The models are handled properly by directory and written to such directories correctly
-        for t in setup_templates:
-            template = self.setup_environment.get_template(t)
+        for t in templates:
+            template = self.environment.get_template(t)
             self.write_model(template, config)
-
-        # for t in final_templates:
-        #     template = self.final_environment.get_template(t)
-        #     self.write_model(config, template)
