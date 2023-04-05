fecdccb2fa141561b2cb90fdf7ff0dfe970c1fa3
Brett <bmiller@om1.com>
2023-04-05 17:00:58 -0400

    unit testing

--- src/om1/templater.py
+++ src/om1/templater.py
@@ -3,38 +3,34 @@ import os
 from jinja2 import Environment
 
 
-class Templater:
+def write_model(template, config):
+    """
+    Creates a model from a template and config
+    :param template: template file for the model
+    :param config: config file
+    :return: name of the new model (its filename)
+    """
 
-    def __init__(self, env: Environment):
-        self.environment = env
+    # Prefix actual filename with variable_name
+    variable_name = config.get("variable_name").lower()
+    substr = "/_"
+    idx = template.filename.index(substr)
+    filename = "../../out/" + template.filename[:idx + 1] + variable_name + template.filename[idx + 1:]
 
-    def write_model(self, template, config):
-        """
-        Creates a model from a template and config
-        :param template: template file for the model
-        :param config: config file
-        :return: name of the new model (its filename)
-        """
+    content = template.render(
+        config=config
+    )
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, mode="w", encoding="utf-8") as message:
+        message.write(content)
+        print(f"... wrote {filename}")
 
-        # Prefix actual filename with variable_name
-        variable_name = config.get("variable_name").lower()
-        substr = "/_"
-        idx = template.filename.index(substr)
-        filename = "../../out/" + template.filename[:idx + 1] + variable_name + template.filename[idx + 1:]
+    return filename
 
-        content = template.render(
-            config=config
-        )
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, mode="w", encoding="utf-8") as message:
-            message.write(content)
-            print(f"... wrote {filename}")
 
-        return filename
+def write_models(config: dict, env: Environment, writer: write_model = write_model):
+    templates = env.list_templates()
 
-    def write_models(self, config):
-        templates = self.environment.list_templates()
-
-        for t in templates:
-            template = self.environment.get_template(t)
-            self.write_model(template, config)
+    for t in templates:
+        template = env.get_template(t)
+        writer(template, config)
--- tests/om1/test_templater.py
+++ tests/om1/test_templater.py
@@ -1,15 +1,28 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 from jinja2 import Environment
 
-from src.om1.templater import Templater
-
-
-def test_init() -> None:
-    expected_env = MagicMock(Environment)
-    templater = Templater(expected_env)
-    assert templater.environment == expected_env
+from src.om1.templater import write_model, write_models
 
 
 def test_write_models() -> None:
-    print("TODO")
+    # setup
+    def list_templates():
+        return [1,2,3]
+
+    def get_template(t):
+        return t
+
+    mock_env = MagicMock(Environment)
+    mock_writer = MagicMock(write_model)
+    mock_config = MagicMock(dict)
+    mock_env.list_templates = list_templates
+    mock_env.get_template = get_template
+
+    # test
+    write_models(mock_config, mock_env, mock_writer)
+
+    # verify
+    calls = [call(1, mock_config), call(2, mock_config), call(3, mock_config)]
+    mock_writer.assert_has_calls(calls)
+
